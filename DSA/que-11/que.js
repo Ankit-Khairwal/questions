0e1f279e392119630a45952bfcddece45b56dc02
Ankit <khairwalankit7@gmail.com>
2025-04-11 21:10:48 +0530

    add

---
+++ DSA/que-11/que.js
@@ -0,0 +1,2 @@
+"question 1: https://leetcode.com/problems/longest-consecutive-sequence/submissions/1603770048";
+"question 2:https://leetcode.com/problems/remove-duplicates-from-sorted-array/submissions/1603770968";
